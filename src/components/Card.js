066f59b1c3ab504be644517bade2b104556e4908
jeradamundsen <jerad.amundsen@gmail.com>
2022-07-05 13:03:28 -0600

    added images and data

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,12 @@
 import React from "react" 
 
-export default function Card(){
+export default function Card(props){
   return(
-    <div>
-      
-    </div>
+    <div className="card">
+    <img src={`../images/${props.img}`} className="card--image" />
+    <p className="card--location">location {props.googleMapsUrl} <span>view on Google Maps</span></p>
+    <p className="card--title">{props.title}</p>
+    
+</div>
   )
 }
--- src/data.js
+++ src/data.js
@@ -1,12 +1,65 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   {
-      title: "Mount Fuji",
-      location: "Japan",
-      googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
-      startDate: "12 Jan, 2021",
-      endDate: "24 Jan, 2021",
-      description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
-      imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+      title: "The Parthenon",
+      location: "Athens, Greece",
+      googleMapsUrl: "https://goo.gl/maps/3WQPaXx6nFNSr3QP9",
+      startDate: "28 Sept, 2013",
+      
+      description: "Restored, landmark temple ruins on archaeological site with iconic Doric columns built from 447 BC.",
+      imageUrl: "../images/athensgreece.jpeg"
+  },
+  {
+    title: "St Marks Basilica",
+    location: "Venice, Italy",
+    googleMapsUrl: "https://goo.gl/maps/pst6D373f9PaKU4v9",
+    startDate: "July 20, 2005",
+    description: "Iconic cathedral with a cavernous gilded interior, myriad mosaics & an on-site museum.",
+    imageUrl: "../images/venice.jpeg"
+  },
+  {
+    title: "Lunch in Capri",
+    location: "Southern Italy",
+    googleMapsUrl: "https://goo.gl/maps/gKnMdaCncUvJJTQq7",
+    startDate: "Sept 22, 2013",
+    description: "Capri is a municipality, in the Metropolitan City of Naples, situated on the island of Capri in Italy. It comprises the centre and East of the island, while the West belongs to Anacapri.",
+    imageUrl: "../images/capri.jpeg"
+  },
+  {
+    title: "Santorini",
+    location: "Greece",
+    googleMapsUrl: "https://goo.gl/maps/WDsE99QqP856NL296",
+    startDate: "Sept 27, 2013",
+    description: "Fira is the capital of Santorini, a Greek island in the Aegean Sea. The Museum of Prehistoric Thira displays items from the destroyed Bronze Age settlement of Akrotiri, including wall paintings and ceramics. Nearby, the whitewashed Orthodox Metropolitan Cathedral has frescoes by local artist Christoforos Asimis. Fira’s clifftop location affords views of Nea Kameni, a still-active volcanic island to the west.",
+    imageUrl: "../santorini2.jpeg"
+  },
+  {
+    title: "Campidoglio",
+    location: "Rome, Italy",
+    googleMapsUrl: "https://goo.gl/maps/rCtdXW6PwvxHbA2PA",
+    startDate: "Sept, 22 2013",
+    description: "Hilltop square designed by Michelangelo, lined with museums & offering views of the Roman forum.
+
+    ",
+    imageUrl: "../images/rome!.jpeg"
+  },
+  {
+    title: "Taormina",
+    location: "Sicily, Italy",
+    googleMapsUrl: "https://goo.gl/maps/DMDqCPZTBSh5Swy68",
+    startDate: "Sept 16, 2013",
+    description: "Taormina is a hilltop town on the east coast of Sicily. It sits near Mount Etna, an active volcano with trails leading to the summit. The town is known for the Teatro Antico di Taormina, an ancient Greco-­Roman theater still used today. ",
+    imageUrl: "../images/southeritaly!.jpeg"
+  },
+  {
+    title: "Windmills of Mykonos",
+    location: "Mykonos, Greece",
+    googleMapsUrl: "https://goo.gl/maps/Y5AngxNoGqcuLgGt8",
+    startDate: "Sept 24, 2013",
+    description: "Famous for its nightlife, this tiny island is home to 16th-century windmills, beaches & dance clubs.",
+    imageUrl: "../images/mykonos.jpeg"
   }
+
+
+
 ]
